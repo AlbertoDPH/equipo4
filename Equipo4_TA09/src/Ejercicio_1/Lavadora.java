34a71e5dc1ad73dfc54199a4f482f89f76dbbb9e
alberto <neok2001.adp@gmail.com>
2023-03-15 11:08:55 +0100

    Clase Lavadora terminada

--- Equipo4_TA09/src/Ejercicio_1/Lavadora.java
+++ Equipo4_TA09/src/Ejercicio_1/Lavadora.java
@@ -1,5 +1,45 @@
 package Ejercicio_1;
 
-class Lavadora {
+class Lavadora extends Electrodomestico {
+
+	private int CARGA;
+
+	// Constructores
+	public Lavadora() {
+		super();
+		this.CARGA = 5;
+	}
+
+	public Lavadora(double precio, int peso) {
+		super(precio, peso);
+		this.CARGA = 5;
+	}
+
+	public Lavadora(double precio, String color, char consumo, int peso, int CARGA) {
+		super(precioFinal(CARGA, precio), color, consumo, peso);
+		// TODO Auto-generated constructor stub
+		this.CARGA = CARGA;
+	}
+
+	// Metodos
+	public static double precioFinal(int CARGA, double precio) {
+
+		if (CARGA > 30) {
+			precio += 50;
+		}
+
+		return precio;
+	}
+
+	// Getters
+	public int getCARGA() {
+		return CARGA;
+	}
+
+	@Override
+	public String toString() {
+		return "Lavadora [CARGA=" + CARGA + ", precioBase=" + precioBase + ", color=" + color + ", consumoEnergetico="
+				+ consumoEnergetico + ", peso=" + peso + "]";
+	}
 
 }
--- Equipo4_TA09/src/Ejercicio_1/ejercicio1_App.java
+++ Equipo4_TA09/src/Ejercicio_1/ejercicio1_App.java
@@ -29,12 +29,27 @@ class ejercicio1_App {
 		e3.setConsumoEnergetico('Z');
 		e3.setColor("magenta");
 		System.out.println(e3.toString());
-		
+
 		// Comprobacion setters con valores correctos
 		e3.setColor("ROJO");
 		e3.setConsumoEnergetico('C');
 		System.out.println(e3.toString());
 
+		// Comprobacion lavadora contruct
+		Lavadora l1 = new Lavadora();
+		System.out.println(l1.toString());
+
+		// Constructor precio y peso
+		Lavadora l2 = new Lavadora(40, 18);
+		System.out.println(l2.toString());
+
+		// Constructor con todos los datos
+		Lavadora l3 = new Lavadora(50,"azul",'A',20,8);
+		System.out.println(l3.toString());
+		
+		// Constructor con todos los datos con carga de + de 30
+		Lavadora l4 = new Lavadora(50,"azul",'A',20,35);
+		System.out.println(l4.toString());
 	}
 
 }
