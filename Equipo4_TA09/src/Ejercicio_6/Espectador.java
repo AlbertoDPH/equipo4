c2aaa3eb0aaeb6d3df831520a67c1ffb5055ff47
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-03-15 17:51:04 +0100

    Creado getters, setters y constructor de espectador

--- Equipo4_TA09/src/Ejercicio_6/Espectador.java
+++ Equipo4_TA09/src/Ejercicio_6/Espectador.java
@@ -1,8 +1,50 @@
 package Ejercicio_6;
 
-public class Espectador extends Cine{
+public class Espectador {
 
 	private String nombre;
 	private int edad;
 	private double dinero;
+
+	// Constructores
+	public Espectador() {
+		this.nombre = "Gumersindo";
+		this.edad = 20;
+		this.dinero = 10;
+	}
+
+	
+	public Espectador(String nombre, int edad, double dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+	}
+
+	
+	// Getters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public double getDinero() {
+		return dinero;
+	}
+
+	// Setters
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public void setDinero(double dinero) {
+		this.dinero = dinero;
+	}
+
 }
