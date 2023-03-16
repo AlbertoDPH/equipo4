294d426d0c488cf722be80393922c53b521f22b2
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-03-16 10:17:12 +0100

    Creado generador de nombres aleatorio

--- Equipo4_TA09/src/Ejercicio_6/Espectador.java
+++ Equipo4_TA09/src/Ejercicio_6/Espectador.java
@@ -10,9 +10,9 @@ public class Espectador {
 
 	// Constructores
 	public Espectador() {
-		this.nombre = "Gumersindo";
+		this.nombre = generadorNombre();
 		this.edad = generadorEdad();
-		this.dinero = 10;
+		this.dinero = generadorDinero();
 	}
 
 	public Espectador(String nombre, int edad, double dinero) {
@@ -54,13 +54,40 @@ public class Espectador {
 
 	// Metodos
 
-	private int generadorEdad() {
+	private int generadorEdad() { // Generador de edad aleatoria entre 5 y 80 años
 		int min = 5;
 		int max = 80;
 		Random edadRandom = new Random();
 
 		int edad = edadRandom.nextInt((max - min) + 1) + min;
 		return edad;
+	}
+
+	private double generadorDinero() { // Generador de dinero aleatorio, entre 0 y 20
+		double min = 0;
+		double max = 20;
+		Random dineroRandom = new Random();
+
+		double dinero = dineroRandom.nextDouble((max - min) + 1) + min;
+		dinero = Math.round(dinero * 100.0) / 100.0; // Redondea el resultado a 2 decimales
+		return dinero;
+	}
+
+	private String generadorNombre() { // Generador de nombres aleatorios definidos en una lista
+		String[] nombres = { "Gumersindo", "Jose", "Javi", "Alberto", "Maria", "Marta", "Andrea", "Francisco", "Hugo",
+				"Lucas", "Alejandra", "Montserrat" };
+
+		Random rand = new Random();
+
+		int indiceRandom = rand.nextInt(nombres.length);
+		String nombreRandom = nombres[indiceRandom];
+
+		return nombreRandom;
+	}
+	
+	public Espectador[] generadorEspectadores(int n) {
+		
+		return;
 	}
 
 }
