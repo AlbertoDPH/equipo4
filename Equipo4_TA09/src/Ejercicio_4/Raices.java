ceb14cad6d41c785d07133eb0c6a6c08693deaa8
alberto <neok2001.adp@gmail.com>
2023-03-20 18:59:17 +0100

    Ejercicio4 UD09 Finalizado

--- Equipo4_TA09/src/Ejercicio_4/Raices.java
+++ Equipo4_TA09/src/Ejercicio_4/Raices.java
@@ -1,22 +1,47 @@
 package Ejercicio_4;
 
+/**
+ * App que calcula la raiz cuadrada los cuales son introducidos por el usario.
+ * La app le pide los tres coeficientes por consola
+ * 
+ * @author Alberto, Josep, Javier
+ * 
+ */
 public class Raices {
 
+	// Atributos
 	private double a;
 	private double b;
 	private double c;
 
+	// Constructor de la clase el cual recibe por parámetro los tres coeficientes
 	public Raices(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
 
+	/**
+	 * Calcula el discriminante de la raiz cuadrada
+	 * 
+	 * @param a recibe el coeficiente a
+	 * @param b recibe el coeficiente a
+	 * @param c recibe el coeficiente a
+	 * @return devuelve el calculo del determinante
+	 */
 	public double getDiscriminante(double a, double b, double c) {
 		double discriminante = (Math.pow(b, 2)) - (4 * a * c);
 		return discriminante;
 	}
 
+	/**
+	 * Controla si la raiz cuadrada tiene más de un resultado Para ello se basa en
+	 * si el discriminante es mayor que 0
+	 * 
+	 * @param discriminante recibe el discriminante
+	 * @return devuelve true si tiene más de un resultado o false si no tiene más de
+	 *         un resultado
+	 */
 	public boolean tieneRaices(double discriminante) {
 		if (discriminante > 0)
 			return true;
@@ -24,6 +49,12 @@ public class Raices {
 			return false;
 	}
 
+	/**
+	 * Controla si tiene un único resultado
+	 * 
+	 * @param discriminante
+	 * @return devuelve true si solo tien un resultado o fasle si no tiene
+	 */
 	public boolean tieneRaiz(double discriminante) {
 		if (discriminante == 0)
 			return true;
@@ -31,8 +62,15 @@ public class Raices {
 			return false;
 	}
 
+	/**
+	 * Regoge los resultados los resultados en un Array, tanto si hay uno como dos
+	 * resultado
+	 * 
+	 * @return devuelve un array con los resultados de la raiz
+	 */
 	public double[] getResultados() {
 		double[] resultado = new double[2];
+		
 		double discriminante = getDiscriminante(a, b, c);
 
 		resultado[0] = (-b + Math.sqrt(discriminante)) / (2 * a);
@@ -42,49 +80,25 @@ public class Raices {
 
 	}
 
+	/**
+	 * Nos muestra la posible opciones que son las dos soluciones, una solucion y no
+	 * tiene solución. Muestra el resultado por consola.
+	 */
 	public void calcular() {
 		double[] resultado = getResultados();
+		
 		double discriminante = getDiscriminante(a, b, c);
 
 		if (discriminante > 0) {
-			System.out.println("La ecuación tiene dos soluciones" + "\nPrimer resultado: " + resultado[0] + "\n"
+			System.out.println("La ecuacion tiene dos soluciones" + "\nPrimer resultado: " + resultado[0] + "\n"
 					+ "Seguno resultado: " + resultado[1]);
 		} else if (discriminante == 0) {
-			System.out.println("Tiene una solución\n" + "Solución: " + resultado[0]);
+			System.out.println("Tiene una solucion\n" + "Solución: " + resultado[0]);
 
 		} else {
-			System.out.println("No tiene solución en el conjunto de los reales");
+			System.out.println("No tiene solucion en el conjunto de los reales");
 		}
 
-	}
-
-	@Override
-	public String toString() {
-		return "Raices [coeA=" + a + ", coeB=" + b + ", coeC=" + c + "]";
-	}
-
-	public double getA() {
-		return a;
-	}
-
-	public void setA(double a) {
-		this.a = a;
-	}
-
-	public double getCoeB() {
-		return b;
-	}
-
-	public void setB(double b) {
-		this.b = b;
-	}
-
-	public double getC() {
-		return c;
-	}
-
-	public void setC(double c) {
-		this.c = c;
 	}
 
 }
--- Equipo4_TA09/src/Ejercicio_4/RaizCuadradaApp.java
+++ Equipo4_TA09/src/Ejercicio_4/RaizCuadradaApp.java
@@ -1,14 +1,29 @@
 package Ejercicio_4;
 
+import javax.swing.JOptionPane;
+
 public class RaizCuadradaApp {
 
 	public static void main(String[] args) {
-		
 
-		Raices raiz = new Raices(1, -4, 4);
-		
+		// Ejemplos de Ecuaciones de 2º grado
+		// No tiene solucion en el conjunto de los reales (2,4,6);
+		// La ecuacion tiene dos soluciones (1,8,3);
+		// Tiene una solucion(1,-2,1);
+
+		double a = Double
+				.parseDouble(JOptionPane.showInputDialog(null, "Introduce constante A", "Ecuación 2º grado", 3));
+
+		double b = Double
+				.parseDouble(JOptionPane.showInputDialog(null, "Introduce constante B", "Ecuación 2º grado", 3));
+
+		double c = Double
+				.parseDouble(JOptionPane.showInputDialog(null, "Introduce constante C", "Ecuación 2º grado", 3));
+
+		Raices raiz = new Raices(a, b, c);
+
 		raiz.calcular();
-		
+
 	}
 
 }
