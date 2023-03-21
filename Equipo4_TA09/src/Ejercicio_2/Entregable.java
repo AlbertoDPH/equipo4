2c8064e00e8d368298be11b552e0dee3b252a3c1
JPrimee <javierpsantos99@gmail.com>
2023-03-21 09:36:21 +0100

    Entregable preparada

--- Equipo4_TA09/src/Ejercicio_2/Entregable.java
+++ Equipo4_TA09/src/Ejercicio_2/Entregable.java
@@ -2,4 +2,40 @@ package Ejercicio_2;
 
 public class Entregable {
 	
+	//Attributes
+	private String title;
+	private boolean delivered;
+	private String gender;
+	
+	//Contrsuctors
+	public Entregable() {
+		this.title = "Default title";
+		this.delivered = false;
+		this.gender = "Default gender";
+	}
+
+	//Setters & getters
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Entregable [title=" + title + ", delivered=" + delivered + ", gender=" + gender + "]";
+	}
+	
+	
+	
 }
